03b508bb372a791a639560b8e340e46c667c231b
Weslley Clyton <wes.hinsch@gmail.com>
2024-01-25 15:22:43 -0300

    correcoes prettier no auth module

--- src/auth/models/UserFromJwt.ts
+++ src/auth/models/UserFromJwt.ts
@@ -1,5 +1,5 @@
 export interface UserFromJwt {
-    id: string;
-    email: string;
-    name: string;
-  }
+  id: string;
+  email: string;
+  name: string;
+}
--- src/auth/models/UserToken.ts
+++ src/auth/models/UserToken.ts
@@ -1,3 +1,3 @@
 export interface UserToken {
-    access_token: string;
-  }
+  access_token: string;
+}
